782ec6e94378764fbb5d4f72bdb860cdb307cd2a
Yuriy Shevchenko <150030@mail.ru>
2023-11-20 14:11:13 +1000

    Added perl command to correctly filter result

--- ConfigManager.cpp
+++ ConfigManager.cpp
@@ -55,6 +55,7 @@ ConfigManager ConfigManager::init(
         if (sect.contains("restart")) tmp.restart = sect["restart"].get<inicpp::boolean_ini_t>();
         if (sect.contains("restartThreshold")) tmp.restartThreshold = sect["restartThreshold"].get<inicpp::float_ini_t>();
         if (sect.contains("saveGS")) tmp.newGSFilename = sect["saveGS"].get<inicpp::string_ini_t>();
+        if (sect.contains("savegs")) tmp.newGSFilename = sect["savegs"].get<inicpp::string_ini_t>();
     }
     
     if (!commandLineParameters.sysfilename.empty())
